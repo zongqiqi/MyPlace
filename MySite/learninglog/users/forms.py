2a55d9bebead860e3b9667a47437911d9cf2f53c
zongqiqi <544136329@qq.com>
2018-03-24 21:42:42 +0800

    Site :fix a bug about auth

--- MySite/learninglog/users/forms.py
+++ MySite/learninglog/users/forms.py
@@ -7,23 +7,24 @@ class RegisterForm(forms.Form):
     username=forms.CharField(max_length=255, required=True)
     password =forms.CharField(widget=forms.PasswordInput, required=True)
     password1 =forms.CharField(widget=forms.PasswordInput, required=True)
+    email=forms.EmailField(required=False)
 
     def clean(self):
         username = self.cleaned_data.get('username')
         password = self.cleaned_data.get('password')
         password1 = self.cleaned_data.get('password1')
+        email= self.cleaned_data.get('email')
         try:
-            user=User.objects.create_user(username=username, password=password,is_active=True)
-            user.save()
+            user=User.objects.get(username=username)
+            raise forms.ValidationError("Username must be unique,sonmeone already register this name.")
         except:
-            raise forms.ValidationError("username must be unique,sonmeone already register this name.")
-        user = authenticate(username=username, password=password)
-        if not user or not user.is_active or password!=password1:
-            raise forms.ValidationError("Sorry, that register was invalid. Please try again.")
-        if username.isdigit():
-            raise forms.ValidationError("Sorry, username can't all number")
-        if len(username) < 6 or len(password) <6:
-            raise forms.ValidationError("Sorry, username or password must six charhter or more")
+            if  password!=password1:
+                raise forms.ValidationError("Sorry, not the same password.")
+            if username.isdigit():
+                raise forms.ValidationError("Sorry, username can't all number")
+            if len(password) <6:
+                raise forms.ValidationError("Sorry, password must six charhter or more")
+            user=User.objects.create_user(username=username,password=password,email=email)
         return self.cleaned_data
     def login(self, request):
         username = self.cleaned_data.get('username')
