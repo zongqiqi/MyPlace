53c0f91fd7faafcc00206f8e8dffda9d6747e549
zongqiqi <544136329@qq.com>
2018-04-06 09:17:27 +0800

    deploy

--- MySite/learninglog/collected_static/pic/big/thumbnil.py
+++ MySite/learninglog/collected_static/pic/big/thumbnil.py
@@ -15,7 +15,7 @@ def clipimage(size):#返回需要裁剪区域的坐标（box）
         box = (0, dx / 2, width, width + dx / 2)
     return box
 def image_size_off():
-	path='../smalll/'
+	path='../small-2/'
 	if not os.path.exists(path):
 		os.makedirs(path)
 	pics=glob.iglob('*.jpg')
@@ -24,7 +24,7 @@ def image_size_off():
 		im=Image.open(pic)
 		box = clipimage(im.size)
 		region = im.crop(box)#返回图像某个给定区域。box 是一个 4 元素元组，定义了 left, upper, right, lower 像素坐标
-		size = (250, 250)
+		size = (100, 100)
 		region.thumbnail(size, Image.ANTIALIAS)
 		region.save(path+pic)
 
